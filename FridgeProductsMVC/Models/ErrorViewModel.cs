8dc817958e623a7cf964af67bf159d3178ae8408
AlexHmurtz <alexhmurtz@gmail.com>
2022-07-04 16:41:55 +0300

    Добавьте файлы проекта.

---
+++ FridgeProductsMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FridgeProductsMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
